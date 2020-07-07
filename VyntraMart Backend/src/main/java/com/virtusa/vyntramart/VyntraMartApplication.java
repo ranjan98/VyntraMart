8b03d65b10814f32319d6772b9fe37a8cdc14fb1
Ranjan Goyal <ranjangoyal1@gmail.com>
2020-07-07 19:30:08 +0530

    Backend

---
+++ VyntraMart Backend/src/main/java/com/virtusa/vyntramart/VyntraMartApplication.java
@@ -0,0 +1,14 @@
+package com.virtusa.vyntramart;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VyntraMartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VyntraMartApplication.class, args);
+	}
+
+}
---
+++ VyntraMart Backend/src/test/java/com/virtusa/vyntramart/VyntraMartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.virtusa.vyntramart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VyntraMartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
